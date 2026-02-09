56c1a32ec4b35cb5acfef079e250157b1ceebc88
dhanushkrock@gmail.com <dhanushkrock@gmail.com>
2026-02-09 10:28:33 +0530

    fix: set allowedHosts to true in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['smcc-web.onrender.com']
+    allowedHosts: true
   },
   preview: {
-    allowedHosts: ['smcc-web.onrender.com']
+    allowedHosts: true
   }
 })
