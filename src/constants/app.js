d860217dd191c9ab2e3184dd627dbdfa6f5d9b7c
dhanush oas <dhanushoas@example.com>
2026-03-13 15:35:04 +0530

    SMCC APP

--- src/constants/app.js
+++ src/constants/app.js
@@ -1,1 +1,1 @@
-export const APK_DOWNLOAD_URL = 'https://drive.google.com/file/d/1jXfS1w6qIUtoOiyCMcv_NmJuwMt6hw4_/view?usp=drive_link';
+export const APK_DOWNLOAD_URL = 'https://drive.google.com/file/d/1LxEh1sMwrEf15BQcLhYeEKNUiuaP7z53/view?usp=drive_link';
