4a7671a62c186cfcc16cbdf92adb41277acb3878
dhanush oas <dhanushoas@example.com>
2026-05-21 12:52:54 +0530

    Update APK download URL in constants

--- src/constants/app.js
+++ src/constants/app.js
@@ -1,1 +1,1 @@
-export const APK_DOWNLOAD_URL = 'https://drive.google.com/file/d/1LxEh1sMwrEf15BQcLhYeEKNUiuaP7z53/view?usp=drive_link';
+export const APK_DOWNLOAD_URL = 'https://drive.google.com/file/d/1eHhmn0ygUehv1XR4cQxL8tHksNEdeP8H/view?usp=drive_link';
